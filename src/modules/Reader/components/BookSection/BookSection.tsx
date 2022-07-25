0a579ed806a2c2d1332637a3eb4e9166ac061f79
Zendrian <zendrian@gmail.com>
2022-07-25 17:18:27 -0500

    add React Reader as one option

--- src/modules/Reader/components/BookSection/BookSection.tsx
+++ src/modules/Reader/components/BookSection/BookSection.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { ReactReader } from "react-reader";
 import "./BookSection.scss";
 
 const BookSection: React.FC = () => {
-  return <section className="bookSection"></section>;
+  const [location, setLocation] = useState<string | number>(0);
+  const locationChanged = (epubcifi: string | number) => {
+    console.log(epubcifi)
+    // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
+    setLocation(epubcifi);
+  };
+
+  console.log("rerender")
+
+  return (
+    <section className="bookSection">
+      <ReactReader
+        location={location}
+        showToc={false}
+        locationChanged={locationChanged}
+        url="testbook\V.0.10.epub"
+      />
+    </section>
+  );
 };
 
 export default BookSection;
